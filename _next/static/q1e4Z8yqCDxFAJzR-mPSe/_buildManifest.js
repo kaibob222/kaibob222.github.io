1751778a825ca2e451bbac6194f8327b4ca5fb79
kaibob222 <43156001+kaibob222@users.noreply.github.com>
2022-04-05 00:17:22 +0000

    Deploying to gh-pages from @ kaibob222/CI-CD-@e03ea75b74638af22bee07b39c9e2a4bad16773b 🚀

---
+++ _next/static/q1e4Z8yqCDxFAJzR-mPSe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-84c97a7c0b770c79.js"],"/_error":["static/chunks/pages/_error-e526a695839b5a36.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
